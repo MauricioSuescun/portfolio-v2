1de726409db05e440da62a4b7c28e6248c7b9276
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2023-06-21 01:52:10 -0500

    linting validation

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,19 +17,19 @@ const About = () => {
         <div className="grid w-full grid-cols-4 gap-16 px-32">
           <div className="col-span-3 flex flex-col items-start justify-start ">
             <p className="font-medium dark:text-light">
-              Hi, I'm CodeBucks, a web developer and UI/UX designer with a
+              Hi, Im CodeBucks, a web developer and UI/UX designer with a
               passion for creating beautiful, functional, and user-centered
               digital experiences. With 4 years of experience in the field. I am
-              always looking for new and innovative ways to bring my clients'
+              always looking for new and innovative ways to bring my clients
               visions to life.
             </p>
             <p className="my-4 font-medium dark:text-light">
               I believe that design is about more than just making things look
-              pretty – it's about solving problems and creating intuitive,
+              pretty – its about solving problems and creating intuitive,
               enjoyable experiences for users.
             </p>
             <p className="font-medium dark:text-light">
-              Whether I'm working on a website, mobile app, or other digital
+              Whether Im working on a website, mobile app, or other digital
               product, I bring my commitment to design excellence and
               user-centered thinking to every project I work on. I look forward
               to the opportunity to bring my skills and passion to your next

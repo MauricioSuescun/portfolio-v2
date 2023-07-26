4bb38ac37569465e4cf45eb8204a9ddb0136cc59
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2023-07-25 23:17:39 -0500

    responsive navbar

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ const MotionLink = motion(Link);
 
 const Logo = () => {
   return (
-    <div className="flex items-center justify-center mt-2">
+    <div className="flex items-center justify-center mt-6">
       <MotionLink
         href="/"
         className="w-16 h-16 bg-black rounded-full text-white flex items-center justify-center text-xl border-2 border-solid border-transparent dark:border-light"

09266f4da624973786bac064b23a1557175fb1f0
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2023-07-26 00:50:47 -0500

    responsive navbar

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -6,13 +6,13 @@ import { motion } from "framer-motion";
 const MotionLink = motion(Link);
 const HireMe = () => {
   return (
-    <div className="fixed right-4 bottom-[-12rem] flex items-center justify-center overflow-hidden md:right-1 md:left-auto md:top-[-15rem] md:w-32 md:bottom-auto">
-      <div className="w-48 h-auto flex items-center justify-center relative">
+    <div className="fixed right-4 bottom-[-12rem] flex items-center justify-center overflow-hidden md:right-1 md:left-auto md:top-[-15rem] md:bottom-auto md:w-32 md:absolute">
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-32">
         <SvgComponent
           className={"fill-black animate-spin-slow dark:fill-light"}
         />
         <MotionLink
-          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border-2 border-solid border-dark w-20 h-20 font-semibold rounded-full  hover:bg-light hover:text-dark dark:bg-light dark:text-black hover:dark:bg-dark hover:dark:text-light hover:dark:border-light"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border-2 border-solid border-dark w-20 h-20 font-semibold rounded-full  hover:bg-light hover:text-dark dark:bg-light dark:text-black hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:w-12 md:h-12 md:text-[10px]"
           href="mailto:mautorres15@hotmail.com"
           whileHover={{
             backgroundColor: [
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,7 +62,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="w-full px-32 pt-4 font-medium flex items-center justify-between dark:text-light">
+    <header className="w-full px-32 pt-4 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8">
       <button
         className="flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}
@@ -222,7 +222,7 @@ const Navbar = () => {
         </motion.div>
       ) : null}
 
-      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+      <div className="absolute left-[50%] top-2 translate-x-[-50%] hidden">
         <Logo />
       </div>
     </header>
